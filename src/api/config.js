311393b603e718381c1632654a4c8d5527222006
Juan <juan.mieres.s@gmail.com>
2018-11-03 02:42:15 -0300

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+  appname: 'platzimusic',
+  apiKey: 'e55dc512838cce5b8461274ef4407b6f',
+  secret: '53ffb4e31d4c57d1a37b8a5e4f8a43f1',
+  registeredTo: 'juanmieres'
+}
---
+++ src/components/Artist.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>
+    <h2>
+      <a v-bind:href="artist.url" target="_blank">{{ artist.name }}</a>
+    </h2>
+    <img v-bind:src="artist.image[2]['#text']">
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'artist',
+  props: ['artist']
+}
+</script>
+
+<style scoped>
+li {
+  display: block;
+  margin: 10px 0;
+}
+</style>
